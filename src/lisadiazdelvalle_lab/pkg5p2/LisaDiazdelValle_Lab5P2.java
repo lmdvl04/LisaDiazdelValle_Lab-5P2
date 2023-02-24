183a5b5c62d5c2957b3da7939bc61f409332df8e
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-02-24 14:52:18 -0600

    menu y agregar

--- src/lisadiazdelvalle_lab/pkg5p2/LisaDiazdelValle_Lab5P2.java
+++ src/lisadiazdelvalle_lab/pkg5p2/LisaDiazdelValle_Lab5P2.java
@@ -14,7 +14,8 @@ public class LisaDiazdelValle_Lab5P2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu m = new Menu ();
+        m.setVisible(true);
     }
     
 }
---
+++ src/lisadiazdelvalle_lab/pkg5p2/Personajes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lisadiazdelvalle_lab.pkg5p2;
+
+/**
+ *
+ * @author HTS
+ */
+public class Personajes {
+    
+}
